8a62db676f18418359fd602379a2ecd925bb0f05
nickmille96 <nickmille96@gmail.com>
2023-04-06 14:05:25 -0400

    init

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,66 @@
 import csv, os
 
-file_load="resources/budget_data.csv"
-file_output="analysis/budget_output.csv"
 
-totalmonths=0
-totalnet=0
+
+total=0 
+profitloss=0 
+value=0
+monthchange=0 
+profits=[]
+dates= []
+
+file_load= os.path.join(r"C:\Users\nickm\OneDrive\Desktop\repos\python_challenge\PyBank\analysis\budget_data.csv")
+file_output=os.path.join(r"C:\Users\nickm\OneDrive\Desktop\repos\python_challenge\PyBank\analysis\analysis_data.csv")
 
 with open (file_load) as file:
     reader=csv.reader(file)
     header=next(reader)
     print (header)
-    firstrow=next(reader)
-    
-    totalmonths += 1
-    totalnet += int(firstrow[1])
-    previousnet=int(firstrow[1])
-    for row in reader:
-        totalmonths += 1
-        totalnet += int(firstrow[1])
 
-        
+    for row in reader:
+        #dates
+        dates.append(row[0])
+        firstrow = next(reader)
+        profitloss += int(firstrow[1])
+        total += 1
+        value =int(firstrow[1])
+
+        #calculate month change
+        monthchange= int(row[1])-value
+        profits.append(monthchange)
+        value =int(row[1])
+        total += 1
+
+        #change in profit and loss
+        profitloss= profitloss +int(row[1])
+
+    #increace in profits
+    profitincrease= max(profits)
+    greatestindex= profits.index(profitincrease)
+    greatestdate= dates[greatestindex]
+
+    #decreace in profits
+    greatestdecrease= min(profits)
+    lowestindex= profits.index(greatestdecrease)
+    lowestdate= dates[lowestindex]
+
+    #calculate average change
+    averagechange=sum(profits)/len(profits)
+#print
+print("Financial Analysis")
+print("---------------")
+print(f"TotalMonths:{str(total)}")
+print(f"Total: {str(profitloss)}")
+print(f"Average Change: ${averagechange}")
+print (f"Greatest Increase in Profits: ({greatestdate}(${profitincrease})")
+print(f"Greatest Decrease in Profits: ({lowestdate}(${greatestdecrease})")
+
+#output
+with open(file_output, "w") as text:
+    text.write(f"Financial Analysis\n")
+    text.write(f"---------------------\n")
+    text.write(f"Total Months: {total}\n")
+    text.write(f"Total: ${profitloss}\n")
+    text.write(f"Average Change: ${averagechange}\n")
+    text.write(f"Greatest Increase in Profits: {greatestdate}(${profitincrease})")
+    text.write(f"Greatest Decrease in Profits:{lowestdate}(${greatestdecrease})")
---
+++ Pypoll/main.py
@@ -0,0 +1,68 @@
+import csv, os
+
+file_load=(r"C:\Users\nickm\OneDrive\Desktop\repos\python_challenge\Pypoll\analysis\election_data.csv")
+file_output=(r"C:\Users\nickm\OneDrive\Desktop\repos\python_challenge\Pypoll\analysis\analysis_data.csv")
+
+totalvotes= 0
+winnercount= 0
+#Vote count by candidate
+votecount= []
+percentagevote =[]
+canidates=[]
+
+#Loop through reader 
+with open (file_load) as file:
+    reader=csv.reader(file)
+    header=next(reader)
+    print (header)
+
+    for row in reader:
+        totalvotes +=1
+
+#If canidate is not on list add them. Then add votes 
+        if row [2] not in canidates:
+            canidates.append(row[2])
+            index=canidates.index(row[2])
+            votecount.append(1)
+        else:
+            index=canidates.index(row[2])
+            votecount[index] +=1
+            
+
+    for votes in votecount:
+#vote percentage 
+        votepercent= (votes/totalvotes) *100
+        votepercent= round(votepercent)
+        percentagevote.append(votepercent)
+        
+
+#Find the winner 
+
+    winner= max(votecount)
+    index=votecount.index(winner)
+    electionwinner= canidates[index]
+
+#print results 
+
+print(f"Election Results\n")
+print(f"Total Votes: {str(totalvotes)}")
+for i in range(len(canidates)):
+    print(f"{canidates[i]}:{str(percentagevote[i])}({str(votecount[i])})")
+
+print(f"---------------------")
+print(f"Winner:{electionwinner}")
+
+#output 
+with open(file_output, "w") as text:
+    text.write(f"Election Results\n")
+    text.write(f"Total Votes: {str(totalvotes)}")
+    for i in range(len(canidates)):
+        text.write(f"{canidates[i]}:{str(percentagevote[i])}({str(votecount[i])})")
+    text.write(f"---------------------")
+    text.write(f"Winner:{electionwinner}")
+
+
+    
+
+
+    
